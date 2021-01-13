487528679573a17c62606abf14d199d414f60052
vrodedanya <socurahlem@yandex.ru>
2021-01-14 01:16:14 +0300

    Added color attribute

--- include/style.hpp
+++ include/style.hpp
@@ -5,6 +5,7 @@
 
 namespace tbo
 {
+	using byte = unsigned char;
 	class style
 	{
 	public:
@@ -12,6 +13,25 @@ namespace tbo
 		{
 			PIXELS = 0,
 			PERCENT = 2147483648 
+		};
+		class color
+		{
+		public:
+			explicit color(tbo::byte r, tbo::byte g, tbo::byte b, tbo::byte a)
+			{
+				this->r = r;
+				this->g = g;
+				this->b = b;
+				this->a = a;
+			}
+			color() : r(0), g(0), b(0), a(0)
+			{
+			}
+			color& operator = (const color& color) = default;
+			tbo::byte r;
+			tbo::byte g;
+			tbo::byte b;
+			tbo::byte a;
 		};
 	private:
 	public:
@@ -26,7 +46,7 @@ namespace tbo
 			int height;
 
 		} size;
-
+		tbo::style::color color;
 	};
 	int operator&(int num, const style::Measure& mes);
 	int operator|(int num, const style::Measure& mes);
