ba0df84b0a3283e7639271d36b3cf2270f15fcde
vrodedanya <socurahlem@yandex.ru>
2021-01-17 22:56:10 +0300

    Added assert function for testing methods

--- include/test.hpp
+++ include/test.hpp
@@ -25,6 +25,21 @@ namespace tbo
 				tbo::logger::log("test", tbo::logger::HIGH_PRIORITY, "test", tests_count++, "is passed!");
 			}
 		}
+		template<typename RETURN_TYPE, typename CLASS, typename... FUNC_ARGS_B, typename... FUNC_ARGS>
+		static void assert(RETURN_TYPE expectation, RETURN_TYPE(CLASS::*func)(FUNC_ARGS_B...), CLASS* obj, FUNC_ARGS... args)
+		{
+			RETURN_TYPE buffer = (obj->*func)(args...);
+			if (buffer != expectation)
+			{
+				tbo::logger::log("test", tbo::logger::HIGH_PRIORITY, "test", tests_count, "is failed!", "Got", buffer, "instead", expectation);
+				std::terminate();
+			}
+			else
+			{
+				tbo::logger::log("test", tbo::logger::HIGH_PRIORITY, "test", tests_count++, "is passed!");
+			}
+		}
+
 	};
 }
 
